0ab4013ba3c845e3a4afe25bd87c524f789994cc
Kristin Thumstedter <114044021+kristint22@users.noreply.github.com>
2023-01-07 21:31:26 -0700

    Add files via upload

---
+++ hey_world.py
@@ -0,0 +1,11 @@
+whats_good = input("Heyyyyy Worldddd, what's good fam? ")
+
+
+if whats_good == 'good':
+    print("That's awesome, I'm so glad to hear! Keep those good vibez!")
+elif whats_good == 'alright':
+    print("I feel that, sometimes you are alright and that's chill.")
+elif whats_good == 'bad':
+    print("Dang Homie, I'm sorry. That sucks. Keep on going homie, one day you will be thriving like no other")
+else:
+    print("Sorry I don't totally understand, I hope the vibez are well though!")
